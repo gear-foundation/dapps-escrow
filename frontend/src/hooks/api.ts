5f95afad5003e1ddec09fb05727f0b7300ac607b
EugeneWay <relax211@gmail.com>
2023-03-06 19:53:18 +0100

    add ui

---
+++ frontend/src/hooks/api.ts
@@ -0,0 +1,45 @@
+import { useSendMessage } from '@gear-js/react-hooks';
+import { Escrow, Wallet } from 'types';
+import stateMetaWasm from 'assets/wasm/state.meta.wasm';
+import metaTxt from 'assets/meta/meta.txt';
+import { getProgramId } from 'utils';
+import { useMetadata, useWasmMetadata } from './useMetadata';
+import { useReadWasmState } from './useReadWasmState';
+
+function useEscrowMetadata() {
+  return useMetadata(metaTxt);
+}
+
+function useEscrowState<T>(functionName: string, payload?: any) {
+  const { buffer } = useWasmMetadata(stateMetaWasm);
+
+  return useReadWasmState<T>(
+    getProgramId(),
+    buffer,
+    functionName,
+    payload,
+  );
+}
+
+function useEscrow(id: string) {
+  const { state, isStateRead } = useEscrowState<Escrow>('info', id);
+
+  const escrow = id ? state : undefined;
+  const isEscrowRead = id ? isStateRead : true;
+
+  return { escrow, isEscrowRead };
+}
+
+function useWallets() {
+  const { state, isStateRead } = useEscrowState<Wallet[]>('created_wallets');
+  const isWalletsStateRead = state ? isStateRead : true;
+
+  return { wallets: state, isWalletsStateRead };
+}
+
+function useEscrowMessage() {
+  const meta = useEscrowMetadata();
+  return useSendMessage(getProgramId(), meta);
+}
+
+export { useEscrow, useWallets, useEscrowMessage, useEscrowMetadata };
